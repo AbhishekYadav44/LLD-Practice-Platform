fa6296618afd455aa24f6ce1988e338bc2733732
abhishek-yadav-2006 <ydvabhishek447@gmail.com>
2026-09-08 22:32:44 +0530

    authmiddleware

---
+++ backend/src/authmiddleware.ts
@@ -0,0 +1,33 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface AuthRequest extends Request {
+  userId?: string;
+}
+
+const authMiddleware = (req: AuthRequest,res: Response,next: NextFunction) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Authentication token required",
+      });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as { userId: string };
+
+    req.userId = decoded.userId;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expired token",
+    });
+  }
+};
+
+export default authMiddleware;
